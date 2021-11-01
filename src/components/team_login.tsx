b1427ef561768845b80d6c442955a4958feb309d
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2021-11-01 19:51:18 +1100

    edit comment team_login

--- src/components/team_login.tsx
+++ src/components/team_login.tsx
@@ -1,34 +1,34 @@
 import React, { ChangeEvent, useState } from "react";
 
 export default function Login() {
-  /* Do not combine states into an object 
-  ** because one at the time username, password changes.
-  ** If we comebine them. state will rerender too many times which is not necessary
-  */
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
+  /* Do not combine states into an object
+   ** because one at the time username, password changes.
+   ** If we comebine them. state will rerender too many times which is not necessary
+   */
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  // Store value in state on change in form
+  // Store value in state on change in formm
   const handleUserNameChange = (event: ChangeEvent) => {
     event.preventDefault();
-    /* Unlike JS we cant simply use event.target.value 
-    ** event.target returns an HTMLElement. In TS, HTMLElement is broad and doesn't have 'value' property.
-    ** must explicitly define HTMLInputElement (and extend of HTMLElement) which has 'value' property accoridng to TS
-    */
+    /* Unlike JS we cant simply use event.target.value
+     ** event.target returns an HTMLElement. In TS, HTMLElement is broad and doesn't have 'value' property.
+     ** must explicitly define HTMLInputElement (and extend of HTMLElement) which has 'value' property accoridng to TS
+     */
     setUsername((event.target as HTMLInputElement).value);
-  }
+  };
 
   const handlePasswordChange = (event: ChangeEvent) => {
     event.preventDefault();
-    /* Unlike JS we cant simply use event.target.value 
-    ** event.target returns an HTMLElement. In TS, HTMLElement is broad and doesn't have 'value' property.
-    ** must explicitly define HTMLInputElement (and extend of HTMLElement) which has 'value' property accoridng to TS
-    */
+    /* Unlike JS we cant simply use event.target.value
+     ** event.target returns an HTMLElement. In TS, HTMLElement is broad and doesn't have 'value' property.
+     ** must explicitly define HTMLInputElement (and extend of HTMLElement) which has 'value' property accoridng to TS
+     */
     setPassword((event.target as HTMLInputElement).value);
-  }
-  
+  };
+
   // Validate user's input with data in salesforce
-  
+
   return (
     <div className="login flex flex-col w-screen h-screen justify-center items-center bg-pink-500">
       <form className="flex flex-col w-96 h-96 bg-white-200 border-gray-900 border-2 justify-center items-center bg-white rounded-md shadow-2xl">
@@ -56,7 +56,10 @@ export default function Login() {
           ></input>
         </label>
 
-        <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 mt-5 rounded-full w-3/12" type="submit">
+        <button
+          className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 mt-5 rounded-full w-3/12"
+          type="submit"
+        >
           Log in
         </button>
       </form>
