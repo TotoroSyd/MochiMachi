a59b2266d49123d40958c981688dea63acaf6083
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2021-11-04 20:40:54 +1100

    Added product pictures to landing pages

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,12 @@
 import ProductCard from "./ProductCard";
+import sakura_mochi from "../media/Sakura-mochi.jpeg";
+import ichigo_daifuku from "../media/Strawberry-Daifuku.jpeg";
+import warabi_mochi from "../media/warabi-mochi.jpeg";
+import hishi_mochi from "../media/Hishi-Mochi.jpeg";
 
 export default function LandingPage() {
   return (
-    <div className="login">
+    <body className="login">
       <div className="flex flex-col justify-center items-center">
         <section className="h-screen w-screen px-2 py-6">
           <nav>
@@ -20,10 +24,25 @@ export default function LandingPage() {
             </button>
           </div>
         </section>
-        <section className="h-screen w-4/5 bg-pink-200 px-2">
-          <ProductCard></ProductCard>
+        <section className="text-center w-2/5 py-28 text-2xl">
+          Welcome to Mochi Machi - your home for authentic, freshmade Japanese
+          Mochi.
+        </section>
+        <section className="h-screen w-4/5 px-2 grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 gap-y-4">
+            <img src={sakura_mochi} alt="sakura-mochi" />
+            <div className="text-center text-5xl">A sign of the season</div>
+          </div>
+          <img src={ichigo_daifuku} alt="strawberry-mochi" />
+          <img src={warabi_mochi} alt="mochi-soybean-powder" />
+          <div className="grid grid-cols-1 gap-y-4">
+            <img src={hishi_mochi} alt="hishi-diamond-mochi" />
+            <div className="text-center text-5xl leading-relaxed">
+              A symbol of good fortune
+            </div>
+          </div>
         </section>
       </div>
-    </div>
+    </body>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,13 +2,17 @@ export default function ProductCard() {
   return (
     <div>
       {/* product image */}
-      <span>Product Name: </span>
+      <h2>Product Name: </h2>
+      <h4>Price</h4>
       <span>Description: </span>
       <div>
-        <span>Order</span>
+        <span>QTY</span>
         {/* Quantity box */}
-        <button type="submit" className="button">
-          Purchase
+        <button
+          type="submit"
+          className="button bg-transparent font-bold py-2 px-4 rounded-full"
+        >
+          ADD TO CART
         </button>
       </div>
     </div>
