66b590eb93689fdf2bf50e7891a704fe4e8da537
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2021-11-07 16:12:57 +1100

    Refactor AppTsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import CheckOut from "./components/CheckOutPage";
 
