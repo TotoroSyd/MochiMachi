0ed1d2be0e553e7a35cef30b72def003d621423b
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2021-11-07 16:45:48 +1100

    Add checkout route to Nav

--- src/components/CheckOutPage.tsx
+++ src/components/CheckOutPage.tsx
@@ -1,3 +1,10 @@
+import Nav from "./Nav";
+
 export default function CheckOut() {
-  return <div>this is checkout page</div>;
+  return (
+    <div>
+      <Nav></Nav>
+      <div>this is checkout page</div>
+    </div>
+  );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className="grid grid-cols-2 gap-12">
       <Link to="/">
         <img src={mochi_machi_logo} alt="mochimachilogo" className="m-2 h-14" />
+      </Link>
+      <Link to="/checkout">
+        <p className="text-center">Check out</p>
       </Link>
     </nav>
   );
