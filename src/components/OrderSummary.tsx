8532ea96c16b8ce06398a762231c37af15659ebf
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2021-11-07 21:27:02 +1100

    Added ordersummary componnt to checkout

---
+++ src/components/OrderSummary.tsx
@@ -0,0 +1,9 @@
+export default function OrderSummary() {
+  return (
+    <div>
+      <h1 className="font-semibold">ORDER SUMMARY</h1>
+      <div>something</div>
+      <div className="text-2xl font-semibold">Total:$999</div>
+    </div>
+  );
+}
