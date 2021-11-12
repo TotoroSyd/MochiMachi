794ab816eb9db9bbf8be56d58f0d1c8b591decaf
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2021-11-12 15:35:26 +1100

    Add route to payment component to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import CheckOut from "./components/CheckOutPage";
 import CheckOutReview from "./components/CheckOutReview";
+import Payment from "./components/Payment";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="checkout" element={<CheckOut />} />
+        <Route path="payment" element={<Payment />} />
         <Route path="review" element={<CheckOutReview />} />
       </Routes>
     </div>
