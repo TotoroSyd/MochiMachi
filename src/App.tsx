c64f38fb56a303f5880d8be573586934dae0ba73
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2021-12-31 22:17:30 +1100

    update Delivery

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,26 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import CheckOut from "./components/CheckOutPage";
 import CheckOutReview from "./components/CheckOutReview";
-import Payment from "./components/Payment";
+// import Payment from "./components/Payment";
 import ModalContextProvider from "./contexts/ModalContext";
 import OrderContextProvider from "./contexts/OrderContext";
+import DeliveryContextProvider from "./contexts/DeliveryContext";
+import OrderConfirmation from "./components/OrderConfirmation";
 
 function App() {
   return (
     <ModalContextProvider>
       <OrderContextProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="checkout" element={<CheckOut />} />
-            <Route path="payment" element={<Payment />} />
-            <Route path="review" element={<CheckOutReview />} />
-          </Routes>
-        </div>
+        <DeliveryContextProvider>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="checkout" element={<CheckOut />} />
+              <Route path="orderconfirmation" element={<OrderConfirmation />} />
+              <Route path="review" element={<CheckOutReview />} />
+            </Routes>
+          </div>
+        </DeliveryContextProvider>
       </OrderContextProvider>
     </ModalContextProvider>
   );
--- src/components/CheckOutPage.tsx
+++ src/components/CheckOutPage.tsx
@@ -1,7 +1,7 @@
 import Nav from "./Nav";
 import { useContext } from "react";
 import { OrderContext, IOrderProps } from "../contexts/OrderContext";
-import Shipping from "./Shipping";
+import Delivery from "./Delivery";
 import OrderSummary from "./OrderSummary";
 import { Link } from "react-router-dom";
 
@@ -15,20 +15,20 @@ export default function CheckOut() {
   return (
     <div>
       <Nav></Nav>
-      <div className="grid grid-cols-3 gap-10 px-28 py-6">
+      <div className="grid grid-cols-3 gap-10 px-28">
         <form className="col-span-2">
-          <Shipping></Shipping>
+          <Delivery></Delivery>
         </form>
         <div className="flex flex-col gap-y-10">
           {<OrderSummary orderArr={orderArray} total={total}></OrderSummary>}
-          <Link to="/review">
+          {/* <Link to="/review">
             <button
               type="submit"
               className="button bg-transparent font-bold py-2 px-4 rounded-full"
             >
               REVIEW
             </button>
-          </Link>
+          </Link> */}
         </div>
       </div>
     </div>
